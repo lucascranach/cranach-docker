d108ff4a18ea1a1f8fe1f45bfd626ea07868a06a
Volker Schaefer <schaefer.volker@googlemail.com>
2020-08-25 16:54:33 +0200

    Added basic HTTP authentication for elasticsearch

--- importer/elasticsearch-import-indices.sh
+++ importer/elasticsearch-import-indices.sh
@@ -8,7 +8,7 @@ echo "using config file $(pwd)/config.cfg"
 echo "\n"
 echo "set max_buckes to 20000"
 
-docker exec -it ${ELASTICSEARCH_CONTAINER} curl -XPUT http://localhost:9200/_cluster/settings -d '
+docker exec -it ${ELASTICSEARCH_CONTAINER} curl --user elastic:password -XPUT http://localhost:9200/_cluster/settings -d '
 {
   "transient": {
     "search.max_buckets": 20000
@@ -28,13 +28,13 @@ do
   echo "file: $file"
   echo "\n"
   echo "deleting index"
-  curl -XDELETE http://localhost:9200/${elasticsearch_index}/
+  curl  --user elastic:password -XDELETE http://localhost:9200/${elasticsearch_index}/
   echo "\n"
   echo "indexing data"
-  curl -s -H "Content-Type: application/x-ndjson" -XPOST localhost:9200/${elasticsearch_index}/_bulk --data-binary "@${file}"
+  curl  --user elastic:password -s -H "Content-Type: application/x-ndjson" -XPOST localhost:9200/${elasticsearch_index}/_bulk --data-binary "@${file}"
   echo "\n"
   echo "set max result window for index"
-  curl -XPUT http://localhost:9200/${elasticsearch_index}/_settings -d '{ "index" : { "max_result_window" : 100000 } }' -H "Content-Type: application/json"
+  curl  --user elastic:password -XPUT http://localhost:9200/${elasticsearch_index}/_settings -d '{ "index" : { "max_result_window" : 100000 } }' -H "Content-Type: application/json"
   echo "\n"
   echo "--------------------"
 done
