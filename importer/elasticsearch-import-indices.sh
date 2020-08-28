dd95660c0963c1d1e9a238730151c2f762496249
Volker Schaefer <schaefer.volker@googlemail.com>
2020-08-28 17:17:52 +0200

    es-importer: Outsorced host, username and password into config

--- importer/elasticsearch-import-indices.sh
+++ importer/elasticsearch-import-indices.sh
@@ -28,13 +28,13 @@ do
   echo "file: $file"
   echo "\n"
   echo "deleting index"
-  curl  --user elastic:password -XDELETE http://${ELASTICSEARCH_HOST}/${elasticsearch_index}/
+  curl  --user ${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD} -XDELETE http://${ELASTICSEARCH_HOST}/${elasticsearch_index}/
   echo "\n"
   echo "indexing data"
-  curl  --user elastic:password -s -H "Content-Type: application/x-ndjson" -XPOST ${ELASTICSEARCH_HOST}/${elasticsearch_index}/_bulk --data-binary "@${file}"
+  curl  --user ${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD} -s -H "Content-Type: application/x-ndjson" -XPOST ${ELASTICSEARCH_HOST}/${elasticsearch_index}/_bulk --data-binary "@${file}"
   echo "\n"
   echo "set max result window for index"
-  curl  --user elastic:password -XPUT http://${ELASTICSEARCH_HOST}/${elasticsearch_index}/_settings -d '{ "index" : { "max_result_window" : 100000 } }' -H "Content-Type: application/json"
+  curl  --user ${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD} -XPUT http://${ELASTICSEARCH_HOST}/${elasticsearch_index}/_settings -d '{ "index" : { "max_result_window" : 100000 } }' -H "Content-Type: application/json"
   echo "\n"
   echo "--------------------"
 done
