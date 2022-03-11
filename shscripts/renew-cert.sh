45721a83d891677d8a814cf97c2b1235633689f6
Volker Schaefer <schaefer.volker@googlemail.com>
2022-03-11 16:58:10 +0100

    Changes order of commands in shellscript

--- shscripts/renew-cert.sh
+++ shscripts/renew-cert.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cd /var/lucascranach/cranach-docker/ && docker-compose stop api
 certbot renew --quiet
+cd /var/lucascranach/cranach-docker/ && docker-compose stop api
 cd /var/lucascranach/cranach-docker/ && sudo docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d api
