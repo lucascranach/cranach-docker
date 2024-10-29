0c80db4bc668cd60aa71b8dd15659de1301b24ea
Volker Schaefer <v.schaefer@klickmeister.de>
2024-10-29 16:10:11 +0100

    Refactor Elasticsearch import script to use variable for Elasticsearch host including http

--- importer/elasticsearch-import-indices.sh
+++ importer/elasticsearch-import-indices.sh
@@ -16,7 +16,7 @@ for i in $import_config
 do
   elasticsearch_index="$(cut -d':' -f1 <<<$i)"
   echo -e "deleting index"
-  curl --user ${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD} -XDELETE http://${ELASTICSEARCH_HOST}/${elasticsearch_index}/
+  curl --user ${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD} -XDELETE ${ELASTICSEARCH_HOST}/${elasticsearch_index}/
 done
 
 echo -e "\n"
@@ -27,7 +27,7 @@ for i in $import_config
 do
   elasticsearch_index="$(cut -d':' -f1 <<<$i)"
   echo -e "creating index and setting mappings"
-  curl --user ${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD} -XPUT http://${ELASTICSEARCH_HOST}/${elasticsearch_index}/ -d '{
+  curl --user ${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD} -XPUT ${ELASTICSEARCH_HOST}/${elasticsearch_index}/ -d '{
     "settings" : { "number_of_shards" : 1, "max_result_window" : 300000, "index.requests.cache.enable": true, "index.queries.cache.enabled": true, "index.mapping.total_fields.limit": 1500 },
     "mappings": {
       "properties": {
@@ -411,7 +411,7 @@ do
   curl  --user ${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD} -s -H "Content-Type: application/x-ndjson" -XPOST ${ELASTICSEARCH_HOST}/${elasticsearch_index}/_bulk --data-binary "@${file}"
   echo -e "\n"
   echo -e "set max result window for index"
-  curl  --user ${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD} -XPUT http://${ELASTICSEARCH_HOST}/${elasticsearch_index}/_settings -d '{ "index" : { "max_result_window" : 100000 } }' -H "Content-Type: application/json"
+  curl  --user ${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD} -XPUT ${ELASTICSEARCH_HOST}/${elasticsearch_index}/_settings -d '{ "index" : { "max_result_window" : 100000 } }' -H "Content-Type: application/json"
   echo -e "\n"
   echo -e "--------------------"
 done
